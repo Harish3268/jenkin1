0c1ad337f830244eca3eda156219ddc0d352a4e2
Harish3268 <harishpadatare100@gamil.com>
2025-05-07 14:36:08 +0530

    master

--- Employee.java
+++ Employee.java
@@ -1,20 +1,27 @@
 package Employee;
-public class Employee{
+
+public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name,int id,double salary){
-        this.name=name;
-        this.id=id;
-        this.salary=salary;
+    String address;
+
+    public Employee(String name, int id, double salary, String address) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.address = address;
     }
-    public void displayInfo(){
-        System.out.println("Employee ID: "+id);
-        System.out.println("Employee Name: "+name);
-        System.out.println("Salary: "+salary);
+
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Salary: " + salary);
+        System.out.println("Address: " + address);
     }
-    public static void main(String[] args){
-        Employee emp1 = new Employee("XYZ",101,50000.0);
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("XYZ", 101, 50000.0, "123 Main St, City, Country");
         emp1.displayInfo();
     }
 }
